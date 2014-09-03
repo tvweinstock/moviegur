07d5f76c646c36b9f3bc07556089b99d532ffe1d
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-03 17:59:36 -0400

    Displaying time in hours

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,10 @@
 class Movie < ActiveRecord::Base
   validates :title, :release_year, presence: true
   validates :duration, numericality: {only_integer: true}
+
+
+  def formatted_duration
+    time_in_hours = duration.to_f / 60.0
+    sprintf("%.2f", time_in_hours)  
+  end
 end
