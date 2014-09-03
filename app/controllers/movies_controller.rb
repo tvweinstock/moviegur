720ee52599be74899eafbc39ae48d12718457517
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-03 10:37:44 -0400

    initial commit

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,51 @@
+class MoviesController < ApplicationController
+
+  before_filter :load_movie, only: [:show, :edit, :update, :destroy]
+  before_action # same as before_filter
+
+  def index
+    @movies = Movie.all    
+  end
+
+  def show    
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to movies_path # browser is going to make a new get request for '/movies'; issue the action
+    else
+      render :new # render will not issue a request to the server again, will look in views folder for associated template
+    end  
+  end
+
+  def edit
+  end
+
+  def update
+    if @movie.update(movie_params)
+      redirect_to movies_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :director)
+  end
+
+  def load_movie
+    @movie = Movie.find(params[:id])
+  end
+
+end
