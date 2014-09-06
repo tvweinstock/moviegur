540ca016768ddbac232f86110d5fe3f553e0332d
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-06 10:51:07 -0400

    Reviews model and controller generated

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,32 @@
+class ReviewsController < ApplicationController
+  before_filter :load_movie
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @review = @movie.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to movies_path, notice: 'Review created successfully'
+    else
+      render 'movies/show'
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:comment, :movie_id)
+  end
+
+  def load_product
+    @movie = Movie.find(params[:movie_id])
+  end
+end
---
+++ db/migrate/20140906123555_remove_actors_from_movies.rb
@@ -0,0 +1,4 @@
+class RemoveActorsFromMovies < ActiveRecord::Migration
+  def change
+  end
+end
