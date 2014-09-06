5d0e3267acb13a7434335a392f05eb36bf00da5f
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-05 22:08:20 -0400

    Adding logic to display 'Signed in as' or 'Login or Sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
   # before_filter: check_country 
 
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
   def ensure_logged_in
     true
   end
